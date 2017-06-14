dbfe8e7606790f901e9e5e60c9570b8dcc01c9da
utk1801 <utk1801@users.noreply.github.com>
2017-06-14 13:04:58 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    d={}
+    count_even=[x for x in list_of_nums if x%2==0]
+    count_odd=[x for x in list_of_nums if x%2==1]
+    d['ODD']=len(count_odd)
+    d['EVEN']=len(count_even)
+    return d
+print solution([1, 2, 3, 5, 8, 9])
